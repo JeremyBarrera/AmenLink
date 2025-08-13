0dacddd5bf4b305accf700d96e49c2663e54678d
Jeremy.Barrera <themaster4678@gmail.com>
2025-08-13 13:55:36 -0300

    gluestack-install

---
+++ app/home.jsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import { ScrollView } from 'react-native-web'
+
+const home = () => {
+  return (
+    <ScrollView>
+      <View>
+        <Text>home</Text>
+      </View>
+    </ScrollView>
+  )
+}
+
+export default home
